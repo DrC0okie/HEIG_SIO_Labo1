a08c54382f2907315e1f98ca0b892a13442ea406
Jarod <jarod.streckeisen@heig-vd.ch>
2024-10-31 13:49:07 +0100

    fix: statistics order for % from optimum

--- src/main/java/ch/heig/sio/lab1/groupJ/StatisticsOutputFormatter.java
+++ src/main/java/ch/heig/sio/lab1/groupJ/StatisticsOutputFormatter.java
@@ -23,9 +23,9 @@ public class StatisticsOutputFormatter {
                 String.format("%.9f", randomInsert.getMinExecutionTime()),
                 String.format("%.9f", randomInsert.getMaxExecutionTime()),
                 String.format("%.9f", randomInsert.getAvgExecutionTime()),
-                String.format("%.2f%s", randomInsert.getAvgPercentageAboveOptimal(),"%"),
                 String.format("%.2f%s", randomInsert.getMaxPercentageAboveOptimal(),"%"),
-                String.format("%.2f%s", randomInsert.getMinPercentageAboveOptimal(),"%"));
+                String.format("%.2f%s", randomInsert.getMinPercentageAboveOptimal(),"%"),
+                String.format("%.2f%s", randomInsert.getAvgPercentageAboveOptimal(),"%"));
 
         System.out.printf(format, "Nearest Insert",
                 nearestInsert.getMinLength(),
@@ -34,19 +34,21 @@ public class StatisticsOutputFormatter {
                 String.format("%.9f", nearestInsert.getMinExecutionTime()),
                 String.format("%.9f", nearestInsert.getMaxExecutionTime()),
                 String.format("%.9f", nearestInsert.getAvgExecutionTime()),
-                String.format("%.2f%s", nearestInsert.getAvgPercentageAboveOptimal(),"%"),
                 String.format("%.2f%s", nearestInsert.getMaxPercentageAboveOptimal(),"%"),
-                String.format("%.2f%s", nearestInsert.getMinPercentageAboveOptimal(),"%"));
-        System.out.printf(format, "Furthest Insert",
+                String.format("%.2f%s", nearestInsert.getMinPercentageAboveOptimal(),"%"),
+                String.format("%.2f%s", nearestInsert.getAvgPercentageAboveOptimal(),"%"));
+
+                System.out.printf(format, "Furthest Insert",
                 furthestInsert.getMinLength(),
                 furthestInsert.getMaxLength(),
                 String.format("%.2f", furthestInsert.getAvgLength()),
                 String.format("%.9f", furthestInsert.getMinExecutionTime()),
                 String.format("%.9f", furthestInsert.getMaxExecutionTime()),
                 String.format("%.9f", furthestInsert.getAvgExecutionTime()),
-                String.format("%.2f%s", furthestInsert.getAvgPercentageAboveOptimal(),"%"),
                 String.format("%.2f%s", furthestInsert.getMaxPercentageAboveOptimal(),"%"),
-                String.format("%.2f%s", furthestInsert.getMinPercentageAboveOptimal(),"%"));
-        System.out.print(separator);
+                String.format("%.2f%s", furthestInsert.getMinPercentageAboveOptimal(),"%"),
+                String.format("%.2f%s", furthestInsert.getAvgPercentageAboveOptimal(),"%"));
+
+                System.out.print(separator);
     }
 }
